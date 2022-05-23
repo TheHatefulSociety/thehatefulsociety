50df1ab39d1c87e5e4df3a973efb2346c3dcc093
SincerelyFaust <hedesmarin@gmail.com>
2022-05-23 13:55:18 +0200

    fix: maintain style consistency

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const Error404: NextPage = () => {
   return (
     <div className="px-0 py-8 mix-blend-difference dark:mix-blend-difference">
-      <main className="flex flex-col items-center text-center justify-center flex-grow min-h-screen p-8 space-y-14">
+      <main className="flex flex-col items-center text-center justify-center flex-grow min-h-screen p-8 space-y-10">
         <p className="text-white dark:text-white text-3xl">
           Can&apos;t find something that hasn&apos;t been built yet.
         </p>
 
-        <h2 className="text-white dark:text-white text-2xl hover:underline">
+        <h2 className="text-white dark:text-white text-2xl hover:underline decoration-3 underline-offset-8">
           <Link href="/">The Hateful Society</Link>
         </h2>
       </main>
--- pages/store.tsx
+++ pages/store.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const Store: NextPage = () => {
   return (
     <div className="px-0 py-8 mix-blend-difference dark:mix-blend-difference">
-      <main className="flex flex-col items-center text-center justify-center flex-grow min-h-screen p-8 space-y-14">
+      <main className="flex flex-col items-center text-center justify-center flex-grow min-h-screen p-8 space-y-10">
         <p className="text-white dark:text-white text-3xl">
           Not coming soon either.
         </p>
 
-        <h2 className="text-white dark:text-white text-2xl hover:underline">
+        <h2 className="text-white dark:text-white text-2xl hover:underline decoration-3 underline-offset-8">
           <Link href="/">Back</Link>
         </h2>
       </main>
